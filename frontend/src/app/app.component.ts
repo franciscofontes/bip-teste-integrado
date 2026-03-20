526421ea7058095c1d3fad9078f6b23a4c96d760
Francisco Fontes <fontes.fj@gmail.com>
2026-03-20 12:28:34 -0300

    criacao projeto angular

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  protected readonly title = signal('frontend');
+}
